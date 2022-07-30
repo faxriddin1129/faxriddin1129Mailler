fd3cd74b7b6c9f338cad00d530ecff31cf08adcc
faxriddin1129 <faxbob1129@gmail.com>
2022-07-30 15:50:43 +0500

    UI UPDATE

--- index.php
+++ index.php
@@ -7,15 +7,16 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <script src="https://cdn.tiny.cloud/1/7ilmz4zph95io0f3h8pbhud6m1rvpa70n0p6f7ohcpdhb64y/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
 </head>
-<body>
+<body class="d-flex align-items-center" style="height: 100vh;background-image: url('https://images.unsplash.com/photo-1541280910158-c4e14f9c94a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80')">
 
 <div class="container">
     <div class="row p-3">
         <div class="col-md-12">
-            <h4 class="text-center text-primary">Faxriddin Miller Service</h4>
+            <h1 class="text-center text-primary mb-3">Faxriddin Mailler Service</h1>
         </div>
-        <div class="col-md-4 card card-body" style="max-height: 335px;">
+        <div class="col-md-3 card card-body" style="max-height: 500px;">
             <div class="form-check form-control" style="min-height: 35px;">
                 <input class="form-check-input" style="margin-left: 20px" type="checkbox" id="check1" name="option1" value="something" checked>
                 <label style="margin-left: 20px" class="form-check-label">All Select</label>
@@ -95,13 +96,20 @@
                 </div>
             </form>
         </div>
-        <div class="col-md-8 card card-body">
-            <textarea class="form-control" name="" id="" cols="30" rows="10"></textarea>
+        <div class="col-md-9 card card-body">
+            <textarea class="form-control" name="" id="" cols="30" rows="15"></textarea>
             <button type="button" class="btn btn-primary mt-2">Send Message</button>
         </div>
-        <div class="col-md-4"></div>
     </div>
 </div>
+
+<script>
+    tinymce.init({
+        selector: 'textarea',
+        plugins: 'advlist autolink lists link image charmap preview anchor pagebreak',
+        toolbar_mode: 'floating | media',
+    });
+</script>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 </body>
