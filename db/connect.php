d3003ea75e0c8443b628345d76abdf75d55131cc
faxriddin1129 <faxbob1129@gmail.com>
2022-07-30 14:32:17 +0500

    UI UPDATE

--- db/connect.php
+++ db/connect.php
@@ -1,1 +1,5 @@
 <?php
+
+//db_name: host2365_mailler
+//parol:  6o*zJ796o
+//username host2365_mailler
---
+++ index.php
@@ -0,0 +1,108 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="container">
+    <div class="row p-3">
+        <div class="col-md-12">
+            <h4 class="text-center text-primary">Faxriddin Miller Service</h4>
+        </div>
+        <div class="col-md-4 card card-body" style="max-height: 335px;">
+            <div class="form-check form-control" style="min-height: 35px;">
+                <input class="form-check-input" style="margin-left: 20px" type="checkbox" id="check1" name="option1" value="something" checked>
+                <label style="margin-left: 20px" class="form-check-label">All Select</label>
+            </div>
+            <form style="overflow-x: auto;">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="check1" name="option1" value="something" checked>
+                    <label class="form-check-label">admin@admin.mail.ru</label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="check1" name="option1" value="something" checked>
+                    <label class="form-check-label">admin@admin.mail.ru</label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="check1" name="option1" value="something" checked>
+                    <label class="form-check-label">admin@admin.mail.ru</label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="check1" name="option1" value="something" checked>
+                    <label class="form-check-label">admin@admin.mail.ru</label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="check1" name="option1" value="something" checked>
+                    <label class="form-check-label">admin@admin.mail.ru</label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="check1" name="option1" value="something" checked>
+                    <label class="form-check-label">admin@admin.mail.ru</label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="check1" name="option1" value="something" checked>
+                    <label class="form-check-label">admin@admin.mail.ru</label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="check1" name="option1" value="something" checked>
+                    <label class="form-check-label">admin@admin.mail.ru</label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="check1" name="option1" value="something" checked>
+                    <label class="form-check-label">admin@admin.mail.ru</label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="check1" name="option1" value="something" checked>
+                    <label class="form-check-label">admin@admin.mail.ru</label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="check1" name="option1" value="something" checked>
+                    <label class="form-check-label">admin@admin.mail.ru</label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="check1" name="option1" value="something" checked>
+                    <label class="form-check-label">admin@admin.mail.ru</label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="check1" name="option1" value="something" checked>
+                    <label class="form-check-label">admin@admin.mail.ru</label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="check1" name="option1" value="something" checked>
+                    <label class="form-check-label">admin@admin.mail.ru</label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="check1" name="option1" value="something" checked>
+                    <label class="form-check-label">admin@admin.mail.ru</label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="check1" name="option1" value="something" checked>
+                    <label class="form-check-label">admin@admin.mail.ru</label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="check1" name="option1" value="something" checked>
+                    <label class="form-check-label">admin@admin.mail.ru</label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="check1" name="option1" value="something" checked>
+                    <label class="form-check-label">admin@admin.mail.ru</label>
+                </div>
+            </form>
+        </div>
+        <div class="col-md-8 card card-body">
+            <textarea class="form-control" name="" id="" cols="30" rows="10"></textarea>
+            <button type="button" class="btn btn-primary mt-2">Send Message</button>
+        </div>
+        <div class="col-md-4"></div>
+    </div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+</body>
+</html>
